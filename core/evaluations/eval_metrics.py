5eea02566fc9f779b2eed7623126e4704ba2b90d
Fafa-DL <516451964@qq.com>
2022-05-16 11:41:39 +0800

    Fix topk evaluation bug

--- core/evaluations/eval_metrics.py
+++ core/evaluations/eval_metrics.py
@@ -211,6 +211,13 @@ def evaluate(
         raise ValueError(f'metric {invalid_metrics} is not supported.')
 
     topk = metric_options.get('topk', (1, 5)) # 若不存在则设置为(1,5)
+    
+    # 判断数据集类别是否小于5，若小于5则top5为100%，但为避免索引报错，将topk最大值取类别数
+    if max(topk) > len(results[0]):
+        temp = len(results[0])
+        print(temp)
+        topk = (1,temp)
+    
     thrs = metric_options.get('thrs')         # 不存在为None
     average_mode = metric_options.get('average_mode', 'macro')
 
