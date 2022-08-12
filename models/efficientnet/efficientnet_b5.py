204f1594e2ab73f2a1bce291264d24fe52b33cba
Fafa-DL <516451964@qq.com>
2022-08-12 10:14:34 +0800

    [code]Fixed efficientnet cfg bug.

--- models/efficientnet/efficientnet_b5.py
+++ models/efficientnet/efficientnet_b5.py
@@ -16,7 +16,11 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomResizedCrop', size=224, backend='pillow'),
+    dict(
+        type='RandomResizedCrop',
+        size=456,
+        efficientnet_style=True,
+        interpolation='bicubic'),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
@@ -25,8 +29,11 @@ train_pipeline = [
 ]
 val_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', size=(256, -1), backend='pillow'),
-    dict(type='CenterCrop', crop_size=224),
+    dict(
+        type='CenterCrop',
+        crop_size=456,
+        efficientnet_style=True,
+        interpolation='bicubic'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='Collect', keys=['img'])
