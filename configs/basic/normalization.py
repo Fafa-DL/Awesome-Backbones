216b5a99e4098f236bae1290a9f9c06bdc6f44e2
Fafa-DL <516451964@qq.com>
2023-08-15 23:54:24 +0800

    bug: fixed LN2d bug

--- configs/basic/normalization.py
+++ configs/basic/normalization.py
@@ -50,12 +50,20 @@ class LayerNorm2d(nn.LayerNorm):
         super().__init__(num_channels, **kwargs)
         self.num_channels = self.normalized_shape[0]
 
-    def forward(self, x):
+    def forward(self, x, data_format='channel_first'):
         assert x.dim() == 4, 'LayerNorm2d only supports inputs with shape ' \
             f'(N, C, H, W), but got tensor with shape {x.shape}'
-        return F.layer_norm(
-            x.permute(0, 2, 3, 1), self.normalized_shape, self.weight,
-            self.bias, self.eps).permute(0, 3, 1, 2)
+        if data_format == 'channel_last':
+            x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias,
+                             self.eps)
+        elif data_format == 'channel_first':
+            x = x.permute(0, 2, 3, 1)
+            x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias,
+                             self.eps)
+            # If the output is discontiguous, it may cause some unexpected
+            # problem in the downstream tasks
+            x = x.permute(0, 3, 1, 2).contiguous()
+        return x
 
 def LN2d(*args, **kwargs):
     return LayerNorm2d
